3d44869cdcf92c5815aaf7b94c9b36a6bdab1378
Leigh Brown <leigh@solinno.co.uk>
2023-06-06 14:22:16 +0100

    Add mos_fread().
    Add mos_fread() MOS API function so that we can read in blocks
    rather than bytes.

--- src/mos-interface.h
+++ src/mos-interface.h
@@ -47,6 +47,7 @@ extern UINT24 getsysvar24bit(UINT8 sysvar);
 extern UINT8 mos_fopen(char * filename, UINT8 mode); // returns filehandle, or 0 on error
 extern UINT8 mos_fclose(UINT8 fh);					 // returns number of still open files
 extern char	 mos_fgetc(UINT8 fh);					 // returns character from file
+extern UINT24 mos_fread(UINT8 fh, char *buffer , UINT24 btr);	// read from file into buffer
 extern void	 mos_fputc(UINT8 fh, char c);			 // writes character to file
 extern UINT8 mos_feof(UINT8 fh);					 // returns 1 if EOF, 0 otherwise
 
